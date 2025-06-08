42395c8b68c46570b09728b56b137e045ec0305c
AliHassan2712 <your-email@example.com>
2025-06-08 12:07:22 +0300

    refactor: update import statements for components and adjust routing paths for clarity

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,19 +1,19 @@
-//react
 import { Navigate } from "react-router-dom";
 import { lazy } from "react";
 
-// Lazy-loaded Components
-const LoginForm = lazy(() => import("../features/components/LoginForm"));
-const RegisterForm = lazy(() => import("../features/components/Register"));
-const ForgetPassword = lazy(() => import("../features/components/ForgetPassword"));
-const VerificationCode = lazy(() => import("../features/components/VerificationCode"));
-const ResetPassword = lazy(() => import("../features/components/ResetPassword"));
-const OAuthSuccess = lazy(() => import("../features/components/OAuthSuccess"));
+// Components
+import { LoginForm } from "../features/components/LoginForm";
+import { RegisterForm } from "../features/components/Register";
+import { ForgetPassword } from "../features/components/ForgetPassword";
+import { VerificationCode } from "../features/components/VerificationCode";
+import { ResetPassword } from "../features/components/ResetPassword";
 
-// Lazy-loaded Pages
+
+// Pages (lazy loaded)
 const Main = lazy(() => import("../pages/Main"));
 const About = lazy(() => import("../pages/About"));
 const Auth = lazy(() => import("../pages/Auth"));
+const OAuthSuccess = lazy(() => import("../features/components/OAuthSuccess"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 
 export const PATH = {
@@ -34,16 +34,17 @@ export const routers = [
     { path: PATH.VerificationCode, element: <VerificationCode /> },
     { path: PATH.ResetPassword, element: <ResetPassword /> },
 
-    {
+    {       
         path: PATH.Auth,
         element: <Auth />,
         children: [
-            { index: true, element: <Navigate to="/login" /> },
+            { index: true, element: <Navigate to="login" /> },
             { path: "login", element: <LoginForm /> },
             { path: "register", element: <RegisterForm /> },
         ],
     },
 
     { path: "oauth-success", element: <OAuthSuccess /> },
+
     { path: "*", element: <NotFound /> },
 ];
