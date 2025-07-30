fcf247adb6db8ccbd64b7978236a2b45e25d0f79
Mohammed <mohammad0592236240@gmail.com>
2025-07-30 14:04:27 +0300

    Dashboard Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import Loading from "./components/Loading";
+import DashboardApp from "./dashboard/App";
+import { MaterialUIControllerProvider } from "./dashboard/context";
 
 function App() {
   return (
     <Suspense fallback={<Loading />}>
-      <Outlet />
+      {/* <Outlet /> */}
+      <MaterialUIControllerProvider>
+
+        <DashboardApp />
+      </MaterialUIControllerProvider>
     </Suspense>
   );
 }
