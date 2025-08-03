bf97fb4b5ad38329097850c525a085f9df38ee2d
Mohammed <mohammad0592236240@gmail.com>
2025-08-03 15:26:33 +0300

    feat: Implement protected dashboard route with role-based access and update routing structure

---
+++ frontend/src/components/ProtectedRoute/ProtectedDashboardRoute.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { PATH } from "../../routes";
+import { Navigate, Outlet } from "react-router-dom";
+import DashboardApp from "../../dashboard/App";
+import { setLayout, useMaterialUIController } from "../../dashboard/context";
+
+const ProtectedDashboardRoute = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const role = user?.role;
+
+    const [controller, dispatch] = useMaterialUIController();
+
+    useEffect(() => {
+        setLayout(dispatch, "dashboard"); // 👈 هذا مهم جداً
+    }, []);
+
+    if (!user || (role !== "admin" && role !== "teacher")) {
+        return <Navigate to={`/${PATH.NotAuth}`} replace />;
+    }
+
+    return (
+        <DashboardApp>
+            <Outlet />
+        </DashboardApp>
+    );
+};
+
+export default ProtectedDashboardRoute;
