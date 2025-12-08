f3e2fd4634f88216d223d3ca010409073f47750a
Ziad Morjan <zmorjan765@gmail.com>
2025-12-08 11:54:46 +0200

    fix: Set default CORS origin to 'http://localhost:3000' if FRONTEND_URL is not defined

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(i18n.init);
 app.use(morgan('dev'));
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL,
+		origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 		credentials: true,
 	}),
 );
